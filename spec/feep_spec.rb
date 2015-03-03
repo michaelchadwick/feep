a14eb22f0e45383b1509659ff29932ada5da0eb2
Michael Chadwick <michael.chadwick@gmail.com>
2015-03-03 00:33:47 -0800

    new attempt at basic test

--- spec/feep_spec.rb
+++ spec/feep_spec.rb
@@ -3,24 +3,18 @@ require 'spec_helper'
 describe Feep do
   subject { Feep.new }
   
-  describe '#basic_stuff' do
-    it 'has a version number' do
-      expect(Feep::VERSION).not_to be nil
-    end
-  end
-  
   describe '#play_sound' do
     options = {
-      :freq_or_note => "440.000", 
-      :waveform => "sine", 
+      :freq_or_note => '440.000', 
+      :waveform => 'sine', 
       :volume => 0.5, 
-      :duration => 100, 
-      :save => false
+      :duration => 500, 
+      :save => false,
+      :loud => false
     }
-    let(:feep) { Feep.new(options) }
   
     it 'plays a sound with no options' do
-      expect(:feep).to eq "Beep!"
+      expect(Feep.new(options)).to eq "sound played"
     end
   end
 
